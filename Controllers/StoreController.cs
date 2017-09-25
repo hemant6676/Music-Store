93b503021e584de5e452be8d19be5daad6525fd9
hemant6676 <31753102+hemant6676@users.noreply.github.com>
2017-09-25 00:03:58 -0400

    first commit

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Music_Store_class_1.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Store/Browse
+        public ActionResult Browse(String genre)
+        {
+            // add the selected genre to the viewbag so we can display it in the browse view
+            ViewBag.genre = genre;
+            return View();
+        }
+    }
+
+
+}
